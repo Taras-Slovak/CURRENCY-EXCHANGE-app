e3d5cc94a7c7ab56be1783b65a12850e9ead3b76
Taras Slovak <slovak.taras@gmail.com>
2021-12-20 21:33:57 +0200

    fix bug

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -5,7 +5,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/jason'
+    'Content-Type': 'application/json'
   })
 }
 
