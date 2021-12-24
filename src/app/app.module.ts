c5194527f93ab81171b60442dff70fb4432f0512
Taras Slovak <slovak.taras@gmail.com>
2021-12-24 18:25:44 +0200

    add select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,6 +51,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
+import { SelectComponent } from './components/select/select.component';
 // ????
 
 
@@ -62,6 +63,7 @@ import { MatTreeModule } from '@angular/material/tree';
     DatepickerComponent,
     ChartsComponent,
     PageComponent,
+    SelectComponent,
 
 
   ],
