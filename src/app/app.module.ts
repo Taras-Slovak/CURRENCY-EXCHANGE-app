21d55737b0b35be5f2a7b4e42033ab813a2288fd
Taras Slovak <slovak.taras@gmail.com>
2021-12-24 12:19:26 +0200

    add component buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { ParametersComponent } from './components/parameters/parameters.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParametersComponent
+    ParametersComponent,
+    ButtonsComponent
 
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
 
   ],
   providers: [],
