420379bc915349f259012fab89e08df931e25290
Taras Slovak <slovak.taras@gmail.com>
2021-12-24 12:21:23 +0200

    add component charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { DatepickerComponent } from './components/datepicker/datepicker.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParametersComponent,
     ButtonsComponent,
-    DatepickerComponent
+    DatepickerComponent,
+    ChartsComponent
 
   ],
   imports: [
