b2fcd19f3ee1e9919c7fc5a65104eaba6f484aee
Taras Slovak <slovak.taras@gmail.com>
2021-12-25 13:40:14 +0200

    render chart

--- src/app/components/charts/chart/chart.component.ts
+++ src/app/components/charts/chart/chart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Chart, registerables } from 'chart.js';
+
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
@@ -10,6 +12,35 @@ export class ChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+
+
+    const labels: any = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+    ];
+    const data: any = {
+      labels: labels,
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      }]
+    };
+    const config: any = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+
+    const myChart = new Chart('myChart', config);
   }
+
+
 
 }
