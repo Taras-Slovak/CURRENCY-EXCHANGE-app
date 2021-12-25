a275abaf229f81145ab02ee835a7128953e15b57
Taras Slovak <slovak.taras@gmail.com>
2021-12-25 17:30:00 +0200

    add fake db

---
+++ src/app/Currency.ts
@@ -0,0 +1,9 @@
+export interface Currancy {
+    baseCurrency: string;
+    currency: string;
+    saleRateNB?: number;
+    purchaseRateNB?: number;
+    vsaleRateNB?: number;
+    saleRate?: number;
+    purchaseRate?: number;
+}
