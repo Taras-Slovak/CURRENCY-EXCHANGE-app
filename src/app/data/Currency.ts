6110f218787021ff0ccf22fed478f343a5a231a8
Taras <slovak.taras@gmail.com>
2021-12-26 17:16:00 +0200

    change data

--- src/app/data/Currency.ts
+++ src/app/data/Currency.ts
@@ -1,16 +1,8 @@
-// export interface Currency {
-//     date: any,
-//     bank: any,
-//     baseCurrency?: any,
-//     baseCurrencyLit: any,
-//     exchangeRate: {},
+export interface Currency {
+    r030?: number,
+    txt: string,
+    rate?: number,
+    cc: string,
+    exchangedate?: number;
+}
 
-// }
-
-// // baseCurrency?: number;
-// //     currency: string;
-// //     saleRateNB?: number;
-// //     purchaseRateNB?: number;
-// //     vsaleRateNB?: number;
-// //     saleRate?: number;
-// //     purchaseRate?: number;
--- src/app/data/moak-currency.ts
+++ src/app/data/moak-currency.ts
@@ -1,182 +1,423 @@
 // import { Currency } from "./Currency"
 export const CURRENCY: any = [
-
     {
-        "date": "25.12.2021",
-        "bank": "PB",
-        "baseCurrency": 980,
-        "baseCurrencyLit": "UAH",
-        "exchangeRate": [
-            {
-                "baseCurrency": "UAH",
-                "saleRateNB": 19.7237,
-                "purchaseRateNB": 19.7237
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "AZN",
-                "saleRateNB": 16.0319,
-                "purchaseRateNB": 16.0319
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "BYN",
-                "saleRateNB": 10.8332,
-                "purchaseRateNB": 10.8332
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "CAD",
-                "saleRateNB": 21.2652,
-                "purchaseRateNB": 21.2652
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "CHF",
-                "saleRateNB": 29.6319,
-                "purchaseRateNB": 29.6319,
-                "saleRate": 30.2,
-                "purchaseRate": 28.6
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "CNY",
-                "saleRateNB": 4.281,
-                "purchaseRateNB": 4.281
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "CZK",
-                "saleRateNB": 1.2301,
-                "purchaseRateNB": 1.2301,
-                "saleRate": 1.34,
-                "purchaseRate": 1.14
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "DKK",
-                "saleRateNB": 4.1481,
-                "purchaseRateNB": 4.1481
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "EUR",
-                "saleRateNB": 30.845,
-                "purchaseRateNB": 30.845,
-                "saleRate": 30.95,
-                "purchaseRate": 30.35
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "GBP",
-                "saleRateNB": 36.6176,
-                "purchaseRateNB": 36.6176,
-                "saleRate": 37.5,
-                "purchaseRate": 35.5
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "GEL",
-                "saleRateNB": 8.7225,
-                "purchaseRateNB": 8.7225
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "HUF",
-                "saleRateNB": 0.083414,
-                "purchaseRateNB": 0.083414
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "ILS",
-                "saleRateNB": 8.6482,
-                "purchaseRateNB": 8.6482
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "JPY",
-                "saleRateNB": 0.23848,
-                "purchaseRateNB": 0.23848
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "KZT",
-                "saleRateNB": 0.06264,
-                "purchaseRateNB": 0.06264
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "MDL",
-                "saleRateNB": 1.5298,
-                "purchaseRateNB": 1.5298
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "NOK",
-                "saleRateNB": 3.0801,
-                "purchaseRateNB": 3.0801
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "PLN",
-                "saleRateNB": 6.6719,
-                "purchaseRateNB": 6.6719,
-                "saleRate": 6.82,
-                "purchaseRate": 6.52
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "RUB",
-                "saleRateNB": 0.3709,
-                "purchaseRateNB": 0.3709,
-                "saleRate": 0.39,
-                "purchaseRate": 0.36
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "SEK",
-                "saleRateNB": 2.9923,
-                "purchaseRateNB": 2.9923
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "SGD",
-                "saleRateNB": 20.0623,
-                "purchaseRateNB": 20.0623
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "TMT",
-                "saleRateNB": 7.7755,
-                "purchaseRateNB": 7.7755
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "TRY",
-                "saleRateNB": 2.4593,
-                "purchaseRateNB": 2.4593
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "UAH",
-                "saleRateNB": 1,
-                "purchaseRateNB": 1
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "USD",
-                "saleRateNB": 27.2747,
-                "purchaseRateNB": 27.2747,
-                "saleRate": 27.35,
-                "purchaseRate": 26.95
-            },
-            {
-                "baseCurrency": "UAH",
-                "currency": "UZS",
-                "saleRateNB": 0.0025247,
-                "purchaseRateNB": 0.0025247
-            }
-        ]
+        "r030": 36,
+        "txt": "Австралійський долар",
+        "rate": 19.7013,
+        "cc": "AUD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 124,
+        "txt": "Канадський долар",
+        "rate": 21.2482,
+        "cc": "CAD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 156,
+        "txt": "Юань Женьміньбі",
+        "rate": 4.2763,
+        "cc": "CNY",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 191,
+        "txt": "Куна",
+        "rate": 4.1028,
+        "cc": "HRK",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 203,
+        "txt": "Чеська крона",
+        "rate": 1.2331,
+        "cc": "CZK",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 208,
+        "txt": "Данська крона",
+        "rate": 4.1445,
+        "cc": "DKK",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 344,
+        "txt": "Гонконгівський долар",
+        "rate": 3.4918,
+        "cc": "HKD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 348,
+        "txt": "Форинт",
+        "rate": 0.083114,
+        "cc": "HUF",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 356,
+        "txt": "Індійська рупія",
+        "rate": 0.36291,
+        "cc": "INR",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 360,
+        "txt": "Рупія",
+        "rate": 0.0019183,
+        "cc": "IDR",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 376,
+        "txt": "Новий ізраїльський шекель",
+        "rate": 8.6246,
+        "cc": "ILS",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 392,
+        "txt": "Єна",
+        "rate": 0.23805,
+        "cc": "JPY",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 398,
+        "txt": "Теньге",
+        "rate": 0.063019,
+        "cc": "KZT",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 410,
+        "txt": "Вона",
+        "rate": 0.022947,
+        "cc": "KRW",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 484,
+        "txt": "Мексиканське песо",
+        "rate": 1.3218,
+        "cc": "MXN",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 498,
+        "txt": "Молдовський лей",
+        "rate": 1.5284,
+        "cc": "MDL",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 554,
+        "txt": "Новозеландський долар",
+        "rate": 18.5767,
+        "cc": "NZD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 578,
+        "txt": "Норвезька крона",
+        "rate": 3.0806,
+        "cc": "NOK",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 643,
+        "txt": "Російський рубль",
+        "rate": 0.37005,
+        "cc": "RUB",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 682,
+        "txt": "Саудівський ріял",
+        "rate": 7.2503,
+        "cc": "SAR",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 702,
+        "txt": "Сінгапурський долар",
+        "rate": 20.0786,
+        "cc": "SGD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 710,
+        "txt": "Ренд",
+        "rate": 1.7538,
+        "cc": "ZAR",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 752,
+        "txt": "Шведська крона",
+        "rate": 2.9835,
+        "cc": "SEK",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 756,
+        "txt": "Швейцарський франк",
+        "rate": 29.6146,
+        "cc": "CHF",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 818,
+        "txt": "Єгипетський фунт",
+        "rate": 1.7341,
+        "cc": "EGP",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 826,
+        "txt": "Фунт стерлінгів",
+        "rate": 36.5162,
+        "cc": "GBP",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 840,
+        "txt": "Долар США",
+        "rate": 27.2306,
+        "cc": "USD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 933,
+        "txt": "Білоруський рубль",
+        "rate": 10.8174,
+        "cc": "BYN",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 946,
+        "txt": "Румунський лей",
+        "rate": 6.226,
+        "cc": "RON",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 949,
+        "txt": "Турецька ліра",
+        "rate": 2.3327,
+        "cc": "TRY",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 960,
+        "txt": "СПЗ (спеціальні права запозичення)",
+        "rate": 38.1026,
+        "cc": "XDR",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 975,
+        "txt": "Болгарський лев",
+        "rate": 15.7584,
+        "cc": "BGN",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 978,
+        "txt": "Євро",
+        "rate": 30.8196,
+        "cc": "EUR",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 985,
+        "txt": "Злотий",
+        "rate": 6.6763,
+        "cc": "PLN",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 12,
+        "txt": "Алжирський динар",
+        "rate": 0.19655,
+        "cc": "DZD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 50,
+        "txt": "Така",
+        "rate": 0.31718,
+        "cc": "BDT",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 51,
+        "txt": "Вірменський драм",
+        "rate": 0.055964,
+        "cc": "AMD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 364,
+        "txt": "Іранський ріал",
+        "rate": 0.00064796,
+        "cc": "IRR",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 368,
+        "txt": "Іракський динар",
+        "rate": 0.01864,
+        "cc": "IQD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 417,
+        "txt": "Сом",
+        "rate": 0.32096,
+        "cc": "KGS",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 422,
+        "txt": "Ліванський фунт",
+        "rate": 0.018053,
+        "cc": "LBP",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 434,
+        "txt": "Лівійський динар",
+        "rate": 5.921,
+        "cc": "LYD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 458,
+        "txt": "Малайзійський ринггіт",
+        "rate": 6.4737,
+        "cc": "MYR",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 504,
+        "txt": "Марокканський дирхам",
+        "rate": 2.9501,
+        "cc": "MAD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 586,
+        "txt": "Пакистанська рупія",
+        "rate": 0.1554,
+        "cc": "PKR",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 704,
+        "txt": "Донг",
+        "rate": 0.0011973,
+        "cc": "VND",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 764,
+        "txt": "Бат",
+        "rate": 0.80788,
+        "cc": "THB",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 784,
+        "txt": "Дирхам ОАЕ",
+        "rate": 7.4093,
+        "cc": "AED",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 788,
+        "txt": "Туніський динар",
+        "rate": 9.4268,
+        "cc": "TND",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 860,
+        "txt": "Узбецький сум",
+        "rate": 0.0025247,
+        "cc": "UZS",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 934,
+        "txt": "Туркменський новий манат",
+        "rate": 7.7755,
+        "cc": "TMT",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 941,
+        "txt": "Сербський динар",
+        "rate": 0.26319,
+        "cc": "RSD",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 944,
+        "txt": "Азербайджанський манат",
+        "rate": 16.0319,
+        "cc": "AZN",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 972,
+        "txt": "Сомоні",
+        "rate": 2.4095,
+        "cc": "TJS",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 981,
+        "txt": "Ларі",
+        "rate": 8.7225,
+        "cc": "GEL",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 986,
+        "txt": "Бразильський реал",
+        "rate": 4.8554,
+        "cc": "BRL",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 959,
+        "txt": "Золото",
+        "rate": 49294.19,
+        "cc": "XAU",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 961,
+        "txt": "Срібло",
+        "rate": 626.54,
+        "cc": "XAG",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 962,
+        "txt": "Платина",
+        "rate": 26611.1,
+        "cc": "XPT",
+        "exchangedate": "28.12.2021"
+    },
+    {
+        "r030": 964,
+        "txt": "Паладій",
+        "rate": 53053.92,
+        "cc": "XPD",
+        "exchangedate": "28.12.2021"
     }
-
 ]
