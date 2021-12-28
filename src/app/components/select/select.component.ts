6ebcf3992d9033524f7eec239281f3f10ae9a55e
Taras Slovak <slovak.taras@gmail.com>
2021-12-29 00:30:09 +0200

    add code

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -12,8 +12,9 @@ interface Food {
 export class SelectComponent implements OnInit {
 
   foods: Food[] = [
-    { value: 'steak-0', viewValue: 'Stweak' },
-    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'USD', viewValue: 'Долар США' },
+    { value: 'EUR', viewValue: 'Євро' },
+    { value: 'PLN', viewValue: 'Злотий' },
 
   ];
 
