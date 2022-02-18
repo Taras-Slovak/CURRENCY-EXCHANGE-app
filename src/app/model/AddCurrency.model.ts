9454a33dddd5760a2bc2a0e54953920bb8904969
Taras Slovak <slovak.taras@gmail.com>
2022-02-18 18:47:40 +0200

    refcatoring

---
+++ src/app/model/AddCurrency.model.ts
@@ -0,0 +1,7 @@
+export type AddCurrency = {
+  r030: number, //840
+  txt: string,  //"US Dollar"
+  rate: number, //28.3
+  cc: string, // "USD"
+  exchangedate: string //"18.02.2022"
+}
---
+++ src/app/store/currency.action.ts
@@ -0,0 +1,4 @@
+export class GetCurrency {
+  static readonly type = '[currency] Get Currency';
+
+}
