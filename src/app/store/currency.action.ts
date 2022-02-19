d757fc4a34764f65419ce68baddcd811a8bd610a
Taras Slovak <slovak.taras@gmail.com>
2022-02-19 18:35:02 +0200

    add c

--- src/app/store/currency.action.ts
+++ src/app/store/currency.action.ts
@@ -1,4 +1,7 @@
+import { AddCurrency } from "../model/AddCurrency.model";
+
 export class GetCurrency {
   static readonly type = '[currency] Get Currency';
 
+  constructor(public payload: AddCurrency) { }
 }
